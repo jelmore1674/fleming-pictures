c54f0be1bb0475c94999cb8d7578c2d532612fa2
Justin Elmore <jelmore1674@pm.me>
2021-10-17 21:24:56 -0500

    styled about page, and added twitter to connect page

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,20 @@ const About: NextPage = () => {
 			<Head>
 				<title>Fleming Pictues | About</title>
 			</Head>
-			<h1>About</h1>
+			<h1>Our Story</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
+				cupiditate earum iste impedit iure amet quam labore odit
+				laboriosam facilis ducimus inventore dolore sunt autem accusamus
+				quae similique, ea minima! Sint fugiat voluptatem aut dolores
+				eligendi doloremque minima quis voluptatibus officiis accusamus
+				dolore dolor illum impedit, recusandae perferendis vel
+				consequuntur fugit soluta maxime alias itaque aperiam hic
+				aliquid enim? Ea! Autem quo voluptates placeat rerum aut,
+				tempore sapiente voluptas harum aspernatur qui quibusdam
+				recusandae a? Dignissimos rerum, tempore natus ratione molestias
+				veritatis quam quisquam debitis eos id atque omnis reiciendis.
+			</p>
 			<p>
 				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
 				cupiditate earum iste impedit iure amet quam labore odit
---
+++ pages/connect.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
+const Connect = () => {
+	return (
+		<div className='twitter-container'>
+			<h3>Keep up to date with our Socials</h3>
+			<TwitterTimelineEmbed
+				sourceType='profile'
+				screenName='flemingpjd'
+				theme='dark'
+				options={{ height: '700' }}
+			/>
+		</div>
+	);
+};
+
+export default Connect;
